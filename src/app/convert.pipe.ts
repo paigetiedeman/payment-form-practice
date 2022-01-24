d437140972ac4149ec0fd040e81d6ee5f9904653
Paige Tiedeman <paigetiedeman@gmail.com>
2022-01-23 23:12:54 -0800

    added cases for custom pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -1,15 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'convert'
+  name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-
-  transform(value: any, ...args: unknown[]): unknown {
-    if (!value){
+  transform(value: any, targetUnits: string): any {
+    if (!value) {
       return '';
     }
-    return value * 1.60934;
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        return new Error("Target Unit not supported");
+    }
   }
-
 }
